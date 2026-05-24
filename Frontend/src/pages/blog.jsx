ee5ab4d3152a203365bf5128c78cd0a1ae17aa2c
leonitahoxha1 <lh72891@ubt-uni.net>
2026-05-24 21:00:00 +0200

    blog

---
+++ Frontend/src/pages/blog.jsx
@@ -0,0 +1,55 @@
+function Blog() {
+
+    const blogs = [
+
+        {
+            title: 'The Art of Slow Cooking',
+            category: 'TECHNIQUE',
+            image: 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg',
+            text:
+                'Learn how slow cooking creates deeper flavor and amazing texture in traditional recipes.'
+        },
+
+        {
+            title: 'Healthy Food Ideas',
+            category: 'INGREDIENTS',
+            image: 'https://images.pexels.com/photos/1213710/pexels-photo-1213710.jpeg',
+            text:
+                'Fresh vegetables, healthy bowls and nutritious recipes for everyday meals.'
+        },
+
+        {
+            title: 'Kitchen Tips & Tricks',
+            category: 'KITCHEN',
+            image: 'https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg',
+            text:
+                'Simple kitchen organization and cooking tricks that make food preparation easier.'
+        },
+
+        {
+            title: 'Best Homemade Bread',
+            category: 'BAKING',
+            image: 'https://images.pexels.com/photos/209206/pexels-photo-209206.jpeg',
+            text:
+                'Discover how to bake soft homemade bread with simple ingredients.'
+        },
+
+        {
+            title: 'Easy Pasta Recipes',
+            category: 'DINNER',
+            image: 'https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg',
+            text:
+                'Delicious pasta recipes perfect for lunch and dinner with family.'
+        },
+
+        {
+            title: 'Fresh Summer Salads',
+            category: 'HEALTHY',
+            image: 'https://images.pexels.com/photos/1213710/pexels-photo-1213710.jpeg',
+            text:
+                'Refreshing healthy salads full of flavor and colorful ingredients.'
+        }
+
+    ]
+
+    
